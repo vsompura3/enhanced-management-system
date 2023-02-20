8a65e6838a7fbc1e57f8d6d42cef4cb836cb3f58
coder_17 <tanupamsaha26@gmail.com>
2023-02-20 23:11:35 +0530

    fixed-the-bug

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, SafeAreaView, View } from "react-native";
+import Login from "./components/Login";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Enhanced Management System</Text>
-      <StatusBar style="auto" />
-    </View>
-  )
+    <SafeAreaView style={styles.screen}>
+      <LinearGradient
+        colors={["#1C192D", "#181A20", "#181A21"]}
+        style={styles.linearGradient}
+      >
+        <View style={styles.container}>
+          <Login />
+          <StatusBar style="auto" />
+        </View>
+      </LinearGradient>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? 25 : 0,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
-})
+  linearGradient: {
+    flex: 1,
+  },
+});
---
+++ frontend/components/Login.js
@@ -0,0 +1,110 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput, Image } from "react-native";
+
+export default function Login() {
+  return (
+    <View style={styles.app}>
+      <View style={styles.icons}>
+        <Image style={styles.logo} source={require("../assets/ems-logo.png")} />
+      </View>
+      <View style={styles.signup}>
+        <View style={styles.header}>
+          {/* <Text style={styles.heading}>EMS</Text> */}
+          <Text style={styles.subheading}>Enhanced Management System</Text>
+          <Text style={styles.tag}>"Making lives faster and better."</Text>
+        </View>
+        <View style={styles.input}>
+          <Text style={styles.label}>Your Registration Number</Text>
+          <TextInput style={styles.credential} />
+          <Text style={styles.label}>Your Password</Text>
+          <TextInput style={styles.credential} />
+          <Text style={styles.btn}>Sign In</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  app: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  icons: {
+    flex: 0.3,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+  },
+  logo: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
+    width: 149,
+  },
+  signup: {
+    flex: 1,
+  },
+  header: {
+    flex: 0.3,
+    paddingHorizontal: 24,
+    paddingVertical: 24,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  heading: {
+    fontSize: 36,
+    color: "#fff",
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  subheading: {
+    fontSize: 24,
+    color: "#fff",
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  tag: {
+    fontSize: 18,
+    color: "#fff",
+    textAlign: "center",
+    fontStyle: "italic",
+  },
+  input: {
+    flex: 0.4,
+    paddingHorizontal: 36,
+    paddingVertical: 28,
+    justifyContent: "center",
+    alignContent: "center",
+  },
+  label: {
+    color: "grey",
+    fontSize: 12,
+    marginTop: 8,
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  credential: {
+    width: 336,
+    borderWidth: 3,
+    borderColor: "transparent",
+    borderBottomColor: "grey",
+    color: "#fff",
+    padding: 8,
+  },
+  btn: {
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+    backgroundColor: "#246BFE",
+    color: "#fff",
+    borderRadius: 100,
+    marginTop: 28,
+    padding: 10,
+    fontSize: 16,
+    fontWeight: "bold",
+    height: 48,
+  },
+});
